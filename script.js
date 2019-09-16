1ee61fc3c8948ab542dd33c7e6afbe428f74369e
Maxim Tyamakov <max@tyamakov.ru>
2019-09-16 15:36:27 +0300

    Promise from  public

--- script.js
+++ script.js
@@ -1,51 +1,70 @@
 let body = document.body;
 let url = window.location.toString();
+let date = new Date();
 
-let nameUrl= (url) => {
-  let getUrl = url.split('=');
-  let name = getUrl[1];  
-  if(name == undefined) {
-    name = 'SveSvet';
-   }
-   return name;
+function hello() {
+	setTimeout(function() {
+  let preloader = document.getElementById('preloader');
+  let loader = document.getElementById('loader');
+  if( !preloader.classList.contains('done') )
+  {
+  	preloader.classList.add('done');
+    loader.classList.add('done');
+  }
+  }, 2000);
 }
 
-let name = nameUrl(url);
+hello();
 
-fetch('https://api.github.com/users/' + name)
-  .then(res => res.json())
-  .then(json => {
-    avatar = json.avatar_url;
-    userName = json.name;
-    description = json.bio;
-    url = json.url;
-    let createName = () => {
-      let userName = document.createElement('h1');
-      userName.innerHTML = name;
-      body.appendChild(userName);
+let getUsername = (url) => {
+    let splitOfUrl = url.split('=');
+    let getMyUsername = splitOfUrl[1];
+    if (getMyUsername == undefined) {
+        getMyUsername = 'SveSvet';
     }
-    let createDescription = () => {
-      let userDescription = document.createElement('h3');
-      userDescription.innerHTML = description;
-      body.appendChild(userDescription);
+    return getMyUsername;
+}
+
+let getDate = new Promise((resolve, reject) => setTimeout(() => date ? resolve(date) : reject('Date is not defined'), 2000)
+);
+
+let getRequest = fetch(`https://api.github.com/users/${getUsername(url)}`);
+Promise.all([getRequest, getDate])
+    .then(([request, date]) => {
+    requestForPromise = request;
+    dateForPromice = date;
+})
+    .then(res => requestForPromise.json())
+    .then(user => {
+    userAvatar = user.avatar_url;
+    userBio = user.bio;
+    userUrl = user.url;
+    
+    let addUsername = () => {
+        let user = document.createElement('h1');
+        user.innerHTML = `${getUsername(url)}`;
+        body.appendChild(user);
     }
-    let createAvatar = () => {
-      let userAvatar = document.createElement('img');
-      let newString = document.createElement('br');
-      userAvatar.src = avatar;
-      body.appendChild(userAvatar);
-      body.appendChild(newString);
+    
+    let addBio = () => {
+        const bio = document.createElement('p');
+        bio.innerHTML = `${userBio}`;
+        body.appendChild(bio);
     }
-    let createUrl = () => {
-      let userUrl = document.createElement('a');
-      let text = document.createTextNode('profile');
-      userUrl.appendChild(text);
-      userUrl.href = 'https://github.com/' + name;
-      body.appendChild(userUrl);
+    
+    let addAvatar = () => {
+        const avatar = document.createElement('img');
+        avatar.src = this.userAvatar;
+        body.appendChild(avatar);
     }
-    createName();
-    createDescription();
-    createAvatar();
-    createUrl();
-  })
-  .catch(err => alert('Нет инфы!'))
+    addUsername();
+    addBio();
+    addAvatar();
+})
+.then(res => {
+    const date = document.createElement('h2');
+    date.innerHTML = `${dateForPromise}`;
+    body.appendChild(date);
+    hello();
+})
+.catch(err => console.log('Page is not defined :c'));
